e9d5b9ff143f4a1fb668f28cdee379e8d4bc6451
Thaddeus Meizelis <tmeizeli@triteqlock.com>
2021-05-24 12:36:50 -0500

    Reintroduces the changes to the react-native-device-info fork.

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -177,7 +177,7 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
 
     try {
       appVersion = getPackageInfo().versionName;
-      buildNumber = Integer.toString(getPackageInfo().versionCode);
+      buildNumber = getBuildConfigValue(getReactApplicationContext(), "BUILD_VERSION");
       appName = getReactApplicationContext().getApplicationInfo().loadLabel(getReactApplicationContext().getPackageManager()).toString();
     } catch (Exception e) {
       appVersion = "unknown";
@@ -202,6 +202,28 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
 
     return constants;
   }
+
+    /**
+     * Gets a field from the project's BuildConfig. This is useful when, for example, flavors
+     * are used at the project level to set custom fields.
+     * @param context       Used to find the correct file
+     * @param fieldName     The name of the field-to-access
+     * @return              The value of the field, or {@code null} if the field is not found.
+     */
+    public static String getBuildConfigValue(Context context, String fieldName) {
+        try {
+            Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
+            Field field = clazz.getField(fieldName);
+            return field.get(null).toString();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+          } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+          } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
   @ReactMethod
   public void isEmulator(Promise p) {
